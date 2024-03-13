13db920718b82b23a25cff6ebfdc6309cac2d14e
60936 <studio34pl@gmail.com>
2024-03-14 07:47:58 +0800

    r

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 const mongoose = require('mongoose');
+const User = require('./models/User');
 require('dotenv').config()
 const app = express();
 
@@ -9,15 +10,19 @@ app.use(cors({
   credentials: true,
   origin: 'http://localhost:5173',
 }));
-console.log(process.env.MONGOD_URI);
-mongoose.connect(process.env.MONGOD_URI);
+
+mongoose.connect('mongodb+srv://booking:tonboswimmers%40gmail.com@airbnc.dyqmkoc.mongodb.net/?retryWrites=true&w=majority&appName=airbnc');
+
 app.get('/test', (req, res) => {
   res.json('test ok');
 });
 
 app.post('/register', (req, res) => {
   const {name,email,password} = req.body;
-  res.json({name,email,password});  
+  User.create({
+    name,
+    email,
+    password});  
 })
 app.listen(3000, () =>{
    console.log('Server listening on port 3000...')
