adb28dd85069e785bca90840a7b8decf231ead31
60936 <studio34pl@gmail.com>
2024-03-13 16:52:43 +0800

    r

--- api/index.js
+++ api/index.js
@@ -1,20 +1,23 @@
 const express = require('express');
 const cors = require('cors');
+const mongoose = require('mongoose');
+require('dotenv').config()
 const app = express();
 
 app.use(express.json());
-
 app.use(cors({
   credentials: true,
   origin: 'http://localhost:5173',
 }));
+console.log(process.env.MONGOD_URI);
+mongoose.connect(process.env.MONGOD_URI);
 app.get('/test', (req, res) => {
   res.json('test ok');
 });
-
+//tonboswimmers%40gmail.com
 app.post('/register', (req, res) => {
   const {name,email,password} = req.body;
-  res.json({name,email,password}); 
+  res.json({name,email,password});  
 })
 app.listen(3000, () =>{
    console.log('Server listening on port 3000...')
