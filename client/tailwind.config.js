a661177c47afe2e76f0f2982e95123b51bc86cfd
60936 <studio34pl@gmail.com>
2024-03-16 13:29:41 +0800

    r

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,15 +1,11 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
- content: [
+export default {
+  content: [
     "./index.html",
-  "./src/**/*.{js,ts,jsx,tsx}",
-],
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
   theme: {
-    extend: {
-      colors:{
-        primary: '#F5385D',
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
