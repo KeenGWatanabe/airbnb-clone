df1ce8b0fb4d1a04a061f6451e167133691fdb2b
60936 <studio34pl@gmail.com>
2024-03-26 18:44:13 +0800

    r

--- README.md
+++ README.md
@@ -28,5 +28,5 @@ airbnb-clone\api>yarn add dotenv
 airbnb-clone\api>yarn add bcryptjs
 airbnb-clone\api>yarn add jsonwebtoken
 
-240326: video stopped at 1:26 
-(cookie jwt for email ,password, doc)
+240326: video stopped at 1:29
+(redirect to index page)
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,18 +5,23 @@ import IndexPage from "./pages/IndexPage"
 import LoginPage from './pages/LoginPage';
 import Layout from './Layout';
 import RegisterPage from './pages/RegisterPage';
+import { UserContextProvider } from './UserContext';
 
 axios.defaults.baseURL = 'http://localhost:3000';
 axios.defaults.withCredentials = true;
 function App() {
   return (
-    <Routes>
+    //create context information for all routes
+    <UserContextProvider>
+      <Routes>
         <Route path="/" element={<Layout />}>
           <Route index element={<IndexPage />} /> 
           <Route path='/login' element={<LoginPage />} /> 
           <Route path='/register' element={<RegisterPage />} /> 
         </Route>
     </Routes>
+    </UserContextProvider>
+    
   )
 }
 
